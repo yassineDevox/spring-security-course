9677f505f38ea528aecd7a40b5da4da88eb54e32
Yassine Rassy <yassine.rassy1@gmail.com>
2020-08-23 19:22:32 +0100

    add authorization done

--- src/main/java/course/java/springsec/springsec/HomeRessource.java
+++ src/main/java/course/java/springsec/springsec/HomeRessource.java
@@ -5,10 +5,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeRessource {
-	
-	@GetMapping("")
+
+	@GetMapping("/")
 	String home() {
-		return ("<h1>hello najlaita !!</h1>");
+		return ("<h1>Hello ALL :) !!</h1>");
+	}
+
+	@GetMapping("/user")
+	String user() {
+
+		return ("<h1>Hello USER :) !!</h1>");
+	}
+
+	@GetMapping("/admin")
+	String admin() {
+
+		return ("<h1>Hello ADMIN :) !!</h1>");
 	}
 
 }
--- src/main/java/course/java/springsec/springsec/SpringSecurityConf.java
+++ src/main/java/course/java/springsec/springsec/SpringSecurityConf.java
@@ -2,6 +2,7 @@ package course.java.springsec.springsec;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,8 +17,12 @@ public class SpringSecurityConf extends WebSecurityConfigurerAdapter {
 		//set our configuration for authentication manager
 		auth.inMemoryAuthentication()
 		.withUser("user")
-		.password("pass")
-		.roles("USER");
+		.password("user")
+		.roles("USER")
+		.and()
+		.withUser("admin")
+		.password("admin")
+		.roles("ADMIN");
 	}
 	
 	
@@ -25,6 +30,18 @@ public class SpringSecurityConf extends WebSecurityConfigurerAdapter {
 	PasswordEncoder passwordEncoder(){
 		return NoOpPasswordEncoder.getInstance();
 	}
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("ADMIN","USER")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+							
+	}
+	
 	
 	
 
